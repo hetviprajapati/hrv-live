e264bbb5edf2d71cd6af2c152678f2755f1291ad
Hetvi Prajapati <hetviprajapati114@gmail.com>
2026-08-24 21:17:57 +0530

    Added changes for color and missaliged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1214,7 +1214,7 @@ export default function HrvLivePage() {
               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
             </svg>
 
-            <div className={`alert-text ${connected ? 'vector-connected' : 'vector-offline'}`}>
+            <div className={`alert-text ${demoMode && 'alert-red'} ${connected ? 'vector-connected' : 'vector-offline'}`}>
               {connected ? deviceName : 'CONNECT POLAR DEVICE'}
             </div>
 
